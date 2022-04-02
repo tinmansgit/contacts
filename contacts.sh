fb3c320da34745c6cf5569b433ff17d90d0f0300
Route One Studio <102759745+routeonestudio@users.noreply.github.com>
2022-04-02 12:08:39 -0500

    Add files via upload

---
+++ contacts.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash 
+# Contact list, data stored in .phonelist.txt
+
+PHONELIST=~/.phonelist.txt
+
+if [ $# -lt 1 ] ; then
+	echo "Contact Name? "
+	exit 1
+fi
+
+if [ $1 = "new" ] ; then
+	shift
+	echo $* >> $PHONELIST
+	echo $* added to database
+	exit 0
+fi
+
+if [ ! -s $PHONELIST ] ; then
+	echo "No Contacts in list. "
+	exit 1
+	else
+	grep -i -q "$*" $PHONELIST
+	if [ $? -ne 0 ] ; then
+		echo "Contact not found. "
+		exit 1
+	else 
+		echo " "
+		grep -i "$*" $PHONELIST
+		echo " "
+	fi
+fi
+exit 0
