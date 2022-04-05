bb06136646e4f91e1366c8c27948d2c1c98cd6e1
Route One Studio <102759745+routeonestudio@users.noreply.github.com>
2022-04-05 15:18:42 -0500

    Update contacts.sh

--- contacts.sh
+++ contacts.sh
@@ -1,31 +1,32 @@
-#!/usr/bin/bash .... for FreeBSD change path to /usr/local/bin/bash
-# Contact list, data stored in .phonelist.txt
+#!/usr/bin/bash 
+# FreeBSD change path to /usr/local/bin/bash
+# Contact list data stored in .contact_list.txt
 
-PHONELIST=~/.phonelist.txt
+CONTACT_LIST=~/.contact_list.txt
 
 if [ $# -lt 1 ] ; then
 	echo "Contact Name? "
 	exit 1
 fi
 
-if [ $1 = "new" ] ; then
+if [ $1 = "add" ] ; then
 	shift
-	echo $* >> $PHONELIST
-	echo $* added to database
+	echo $* >> $CONTACT_LIST
+	echo $* " added." 
 	exit 0
 fi
 
-if [ ! -s $PHONELIST ] ; then
-	echo "No Contacts in list. "
+if [ ! -s $CONTACT_LIST ] ; then
+	echo "No contacts added. "
 	exit 1
 	else
-	grep -i -q "$*" $PHONELIST
+	grep -i -q "$*" $CONTACT_LIST
 	if [ $? -ne 0 ] ; then
 		echo "Contact not found. "
 		exit 1
 	else 
 		echo " "
-		grep -i "$*" $PHONELIST
+		grep -i "$*" $CONTACT_LIST
 		echo " "
 	fi
 fi
